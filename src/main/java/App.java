f4662ce7a0c44f0899d0324d2f11a06acb691075
Vladimir Kotov <vladimir.kotov@citadele.lv>
2019-02-28 22:08:36 +0200

    sds

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
 
     static String[][] input;
+    static int[][] adj;
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
@@ -13,8 +15,44 @@ public class App {
 
         input = new String[t][];
         for (int i = 1; i <= t; i++) {
-            input[i-1] = in.nextLine().split(" ");
+            input[i - 1] = in.nextLine().split(" ");
         }
+
+        adj = new int[t][t];
+
+        for (int i = 0; i < t; i++) {
+            for (int j = 0; j < t; j++) {
+                if (i == j) {
+                    adj[i][j] = 0;
+                } else {
+                    adj[i][j] = Scoring.score(input[i], input[j]);
+                }
+                System.out.print(adj[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        List<Integer> visitedList = new ArrayList<>();
+        Set<Integer> visited = new HashSet<>();
+
+        int current = 0;
+        int maxPos;
+
+        do {
+            maxPos = 0;
+            visitedList.add(current);
+            visited.add(current);
+
+            int max = 0;
+            for (int i = 0; i < t; i++) {
+                if (!visited.contains(i) && adj[current][i] > max) {
+                    maxPos = i;
+                    max = adj[current][i];
+                }
+            }
+            current = maxPos;
+        } while (maxPos > 0);
+        
     }
 
     public static String process(BigInteger x, BigInteger y) {
--- src/main/java/Scoring.java
+++ src/main/java/Scoring.java
@@ -3,24 +3,23 @@
  */
 
 public class Scoring {
-    static int score(Slide s1, Slide s2) {
+
+    static int score(String[] s1tags, String[] s2tags) {
         int result = 0;
 
         int common = 0, unique1 = 0, unique2 = 0;
 
-        for (int i1 = 0; i1 < s1.tags.length; i1++)
-        {
-            for (int i2 = 0; i2 < s2.tags.length; i2++)
-            {
-                if (s1.tags[i1] == s2.tags[i2]) {
+        for (int i1 = 2; i1 < s1tags.length; i1++) {
+            for (int i2 = 2; i2 < s2tags.length; i2++) {
+                if (s1tags[i1].equals(s2tags[i2])) {
                     common++;
                     break;
                 }
             }
         }
 
-        unique1 = s1.tags.length - common;
-        unique2 = s2.tags.length - common;
+        unique1 = s1tags.length - common;
+        unique2 = s2tags.length - common;
 
         result = Math.min(common, unique1);
         result = Math.min(result, unique2);
