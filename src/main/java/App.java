2947569a8e817bbfc019db893dca945c6e3a1f52
Vladimir Kotov <vladimir.kotov@citadele.lv>
2019-02-28 21:29:08 +0200

    xxx

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-    static List<Slide> photos = new ArrayList<>();
+    static String[][] input;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
         int t = Integer.valueOf(in.nextLine());
+
+        input = new String[t][];
         for (int i = 1; i <= t; i++) {
-            String[] s = in.nextLine().split(" ");
-
-
-
-            BigInteger x = new BigInteger(s[0]);
-            BigInteger y = new BigInteger(s[1]);
-            String r = process(x, y);
-            System.out.println("Case #" + i + ": " + r);
+            input[i-1] = in.nextLine().split(" ");
         }
     }
 
@@ -31,11 +24,4 @@ public class App {
     public static String process(int x, int y) {
         return x + " " + y;
     }
-
-
-    4
-    H 3 cat beach sun
-    V 2 selfie smile
-    V 2 garden selfie
-    H 2 garden cat
 }
